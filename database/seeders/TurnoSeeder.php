9ce6244399ac5c27575aeab8a008afe67325eb1f
Elber Jesus <equirozc@unprg.edu.pe>
2026-06-21 18:55:32 -0500

    Update TurnoSeeder.php

--- database/seeders/TurnoSeeder.php
+++ database/seeders/TurnoSeeder.php
@@ -10,8 +10,8 @@ class TurnoSeeder extends Seeder
     public function run(): void
     {
         foreach ([
-            ['codigo' => 'MANANA', 'nombre' => 'Mañana', 'hora_inicio' => '08:00', 'hora_fin' => '12:00'],
-            ['codigo' => 'TARDE', 'nombre' => 'Tarde', 'hora_inicio' => '14:00', 'hora_fin' => '18:00'],
+            ['codigo' => 'T001', 'nombre' => 'Mañana', 'hora_inicio' => '08:00', 'hora_fin' => '12:00'],
+            ['codigo' => 'T002', 'nombre' => 'Tarde', 'hora_inicio' => '14:00', 'hora_fin' => '18:00'],
         ] as $turno) {
             Turno::query()->updateOrCreate(
                 ['codigo' => $turno['codigo']],
